7b14cffd22e52b1a2df04fc24c57602c643d1213
minjibnp <minji.bnp@gmail.com>
2020-11-04 18:08:43 +0900

    K01Java

---
+++ quiz/TestClass.java
@@ -0,0 +1,34 @@
+package quiz;
+
+import java.util.Random;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		Random rand = new Random();
+
+		int[] randNum = new int[10];
+
+		for(int i=0; i<randNum.length; i++) {
+			randNum[i] = rand.nextInt(10)+1;
+		}
+
+		for(int i=0; i<randNum.length; i++) {
+			for(int j=0; j<randNum.length; j++) {
+				if(i != j) {
+					if(randNum[i] == randNum[j]) {
+						randNum[i] = rand.nextInt(10)+1;
+						j = 0;
+						i = 0;
+					}
+				}
+			}
+		}
+
+
+		for(int i=0; i<randNum.length; i++) {
+			System.out.println(randNum[i]);
+		}
+	}
+}
